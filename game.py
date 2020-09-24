a0332fab354434860457b65189af0ac78dafb3ec
Stefan Dunstan <67916591+Stefd9@users.noreply.github.com>
2020-09-24 20:02:08 +0100

    Add files via upload

---
+++ game.py
@@ -0,0 +1,152 @@
+import pygame
+import random
+import sys
+
+pygame.init()
+
+#--DISPLAY/GAME BOARD---
+
+board_width = 800
+board_height = 600
+
+player_red_piece = (255,0,0)
+blue_piece = (0,0,255)
+yellow_score = (255, 255, 0)
+
+player_size = 40
+player_pos = [board_width/2, board_height-2*player_size]
+playerX_change = 0
+
+background_color = (0,0,0)
+
+enemy_size = 40
+enemy_pos = [random.randint(0, board_width-enemy_size), 0]
+enemy_list = [enemy_pos]
+
+speed = 10
+
+screen = pygame.display.set_mode((board_width, board_height))
+
+pygame.display.set_caption("Dodge the blocks!")
+
+
+game_over = False
+
+score = 0
+
+clock = pygame.time.Clock()
+
+myFont = pygame.font.SysFont("monospace", 35)
+
+#-----------------
+
+def set_level(score, speed):
+	if score < 20:
+		speed = 5
+	elif score < 40:
+		speed = 8
+	elif score < 60:
+		speed = 12
+	elif score < 90:
+		speed = score/5 + 1
+	return speed
+
+def drop_enemies(enemy_list):
+	delay = random.random()
+	if len(enemy_list) < 10 and delay < 0.1:
+		x_pos = random.randint(0, board_width-enemy_size)
+		y_pos = 0
+		enemy_list.append([x_pos, y_pos])
+
+def draw_enemies(enemy_list):
+	for enemy_pos in enemy_list:
+		pygame.draw.rect(screen, blue_piece, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+
+def update_enemy_positions(enemy_list, score):
+	for idx, enemy_pos in enumerate(enemy_list):
+		if enemy_pos[1] >= 0 and enemy_pos[1] < board_height:
+			enemy_pos[1] += speed
+		else: 
+			enemy_list.pop(idx)
+			score += 1
+	return score
+
+def collision_check(enemy_list, player_pos):
+	for enemy_pos in enemy_list:
+		if detect_collison(enemy_pos, player_pos):
+			return True
+	return False
+
+
+def detect_collison(player_pos, enemy_pos):
+	p_x = player_pos[0]
+	p_y = player_pos[1]
+
+	e_x = enemy_pos[0]
+	e_y = enemy_pos[1]
+
+	if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x+enemy_size)):
+		if (e_y >= p_y and e_y < (p_y + player_size)) or (p_y >= e_y and p_y < (e_y+enemy_size)):
+			return True
+	return False
+
+
+#--Game coding--
+
+while not game_over:
+
+	for event in pygame.event.get():
+
+		if event.type == pygame.QUIT:
+			sys.exit()
+
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				playerX_change = -6
+
+			elif event.key == pygame.K_RIGHT:
+				playerX_change = 6
+
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				playerX_change = 0
+
+	if player_pos[0] <=0:
+		player_pos[0] = 0
+	elif player_pos[0] >= 760:
+		player_pos[0] = 760
+
+	player_pos[0] += playerX_change
+
+	screen.fill(background_color)
+
+	#UPDATES POSITION OF ENEMY
+	
+
+
+	if detect_collison(player_pos, enemy_pos):
+		game_over = True
+		break
+
+	drop_enemies(enemy_list)
+	score = update_enemy_positions(enemy_list, score)
+	speed = set_level(score, speed)
+	
+
+	text = "Score: " + str(score)
+	label = myFont.render(text, 1, yellow_score)
+	screen.blit(label, (board_width-200, board_height-40))
+
+	if collision_check(enemy_list, player_pos):
+		game_over - True
+		break
+
+	draw_enemies(enemy_list)
+
+	pygame.draw.rect(screen, player_red_piece, (player_pos[0], player_pos[1], player_size, player_size))
+
+	clock.tick(30)
+	
+	pygame.display.update()	
+
+#------------------
